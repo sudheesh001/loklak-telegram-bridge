b654eadc6cb87bf6b5dee6bbeec12f0dc0e08fdc
sudheesh001 <sudheesh1995@outlook.com>
2015-10-28 18:59:29 +0530

    Initial barebone for running echo

---
+++ server.py
@@ -0,0 +1,49 @@
+import logging
+import telegram
+from token import TOKEN
+
+
+LAST_UPDATE_ID = None
+
+
+def main():
+    global LAST_UPDATE_ID
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot(TOKEN)
+
+    # This will be our global variable to keep the latest update_id when requesting
+    # for updates. It starts with the latest update_id if available.
+    try:
+        LAST_UPDATE_ID = bot.getUpdates()[-1].update_id
+    except IndexError:
+        LAST_UPDATE_ID = None
+
+    while True:
+        echo(bot)
+
+
+def echo(bot):
+    global LAST_UPDATE_ID
+
+    # Request updates after the last updated_id
+    for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=10):
+        # chat_id is required to reply any message
+        chat_id = update.message.chat_id
+        message = update.message.text.encode('utf-8')
+
+        if (message):
+            # Reply the message
+            print str(chat_id) + ' :: ' + str(message)
+            bot.sendMessage(chat_id=chat_id,
+                            text=message)
+
+            # Updates global offset to get the new updates
+            LAST_UPDATE_ID = update.update_id + 1
+
+
+if __name__ == '__main__':
+    main()
