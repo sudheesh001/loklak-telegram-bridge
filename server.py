6539d9e151aedd12a808102b0528d6d733dfa895
Vladyslav Ovchynnykov <ovd@ovdlaptop.inc>
2015-12-23 21:33:28 +0200

    Added /status and /user functions

--- server.py
+++ server.py
@@ -1,19 +1,16 @@
 import logging
 import telegram
 from token import TOKEN
-from loklak import Loklak
 
-from utils import get_tweet_rating, tweet_reply
-
+from commands import serveOptions, search, status, user
 
 LAST_UPDATE_ID = None
 
-l = Loklak()
-
 commands = {
-    'no-args': ['/start', '/help'],
+    'no-args': ['/start', '/help', '/status'],
     'with-args': ['/search', '/suggest', '/crawler', '/geocode', '/user']
 }
+
 
 def main():
     global LAST_UPDATE_ID
@@ -34,49 +31,6 @@ def main():
     while True:
         echo(bot)
 
-def search(query):
-    """Search Loklak for a specific query.
-
-    Args:
-        query: the query to search for.
-    Returns:
-        search results for query.
-
-    """
-    try:
-        tweets = l.search(query)['statuses']
-        if tweets:
-            tweets.sort(key=get_tweet_rating)
-            tweet = tweets.pop()
-            return tweet_reply(tweet, len(tweets))
-        else:
-            # Try to search for a weaker query by deleting the last word
-            # "An awesome query" -> "An awesome" -> ...
-            query = query.split()[:-1]
-            if query:
-                query = ' '.join(query)
-                search(query)
-            else:
-                return 'Sorry, but I haven\'t found any tweets for your query'
-    except:
-        return 'Something went wrong'
-
-
-def serveOptions():
-    options = """
-            Search API Wrapper which helps to query loklak for JSON results.\n
-            Status API Wrapper for the loklak status check.\n
-            Suggestions API Wrapper , Works better with local loklak instance.\n
-            Crawler API Wrapper on Loklak to crawl for tweets for a particular crawl depth.\n
-            Loklak status check API.\n
-            Geocode API for geolocation based information.\n
-            Loklak API for peers connected on the distributed network.\n
-            Public API to push geojson objects to the loklak server.\n
-            User API to show twitter user information.\n
-            Map Visualization render using Loklak service.\n
-            Markdown conversion API to render markdown as image using Loklak.\n
-            """
-    return options
 
 def stringParse(bot, messageString):
     # String parser functions that are required for the bot.
@@ -91,18 +45,19 @@ def stringParse(bot, messageString):
         if command in commands['no-args']:
             if command == '/start' or command == '/help':
                 return serveOptions()
+            elif command == '/status':
+                return status()
         else:
             if command in commands['with-args']:
                 return 'Sorry, you have to pass some arguments to {}'.format(command)
             else:
                 return 'Sorry, but I don\'t know what to do with {}'.format(command)
-            
-    if command in commands['with-args']:      
+
+    if command in commands['with-args']:
         if command == '/search':
             return search(query)
         elif command == '/status':
-            # do some operations
-            pass
+            return status()
         elif command == '/suggest':
             # do some operations
             pass
@@ -113,10 +68,10 @@ def stringParse(bot, messageString):
             # do some operations
             pass
         elif command == '/user':
-            # do some operations
-            pass
+            return user(query)
     else:
         return 'Sorry, but I don\'t know what to do with {}'.format(command)
+
 
 def echo(bot):
     global LAST_UPDATE_ID
--- utils.py
+++ utils.py
@@ -4,14 +4,40 @@ def get_tweet_rating(tweet):
     """
     return (tweet['retweet_count'] * 2) + tweet['favourites_count']
 
+
 def tweet_reply(tweet, tweets_left):
     """
     Function that returns a tweet from the loklak object
     """
-    reply = '"{message}" - {author} \n\n{link}\n\n and {more} more tweets.'.format(
-        message=tweet['text'].encode('utf-8'),
-        author=tweet['screen_name'].encode('utf-8'),
-        link=tweet['link'].encode('utf-8'),
+    reply = '"{message}" - {author}\n\n{link}\n\n and {more} more tweets.'
+    reply = reply.format(
+        message=tweet['text'],
+        author=tweet['screen_name'],
+        link=tweet['link'],
         more=tweets_left
     )
+    reply = reply.encode('utf-8')
+    return reply
+
+
+def user_reply(user):
+    reply = """
+        {name} - {screen_name}\n
+        {description}
+        Statuses count: {statuses_count}
+        Favourites count: {favourites_count}
+        Followers count: {followers_count}
+        Following: {following_count}
+
+        {img}
+    """
+    reply = reply.format(name=user['name'], screen_name=user['screen_name'],
+                         description=user['description'],
+                         statuses_count=user['statuses_count'],
+                         favourites_count=user['favourites_count'],
+                         followers_count=user['followers_count'],
+                         url=user['url'],
+                         following_count=user['friends_count'],
+                         img=user['profile_image_url'])
+    reply = reply.encode('utf-8')
     return reply
